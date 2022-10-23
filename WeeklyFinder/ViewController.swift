6ef94d0ac8b45a2edcd94399b984036d48aecb42
max-kubasov <kubasov87@gmail.com>
2022-10-23 15:30:46 +0300

    Add func weekly finder

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -46,6 +46,7 @@ class ViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
         self.view.endEditing(true)
     }
     
